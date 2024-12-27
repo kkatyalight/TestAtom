02e5bdcee9c44d26d49eb1805388cc46deb147a9
kkatyalight <katyalight.kl@gmail.com>
2024-12-27 23:20:33 +0300

    upd url 3

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://kkatyalight.github.io/TestAtom/",
+  base: "/TestAtom/",
   plugins: [react()],
 })
